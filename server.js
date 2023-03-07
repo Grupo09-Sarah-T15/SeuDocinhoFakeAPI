8dfbdec184b97c25f5aa08965aed33ed91e04516
MatheusGualtieri <50122650+MatheusGualtieri@users.noreply.github.com>
2023-03-07 15:19:03 -0300

    feat: add new data

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const jsonServer = require("json-server");
 const auth = require("json-server-auth");
 const cors = require("cors");
 const port = process.env.PORT || 3001;
-
 const app = jsonServer.create();
 const router = jsonServer.router("db.json");
 
